490312c436b7c1333fbb3baf87161f58e3186ec6
nafis@aidocbuilder.com <rongdhong@54321>
2025-01-31 15:29:21 +0600

    Window Size updated

--- conftest.py
+++ conftest.py
@@ -4,6 +4,8 @@ from selenium import webdriver
 @pytest.fixture(scope="function")
 def setup():
     driver = webdriver.Chrome()
-    driver.maximize_window()
+    # driver.maximize_window()
+    driver.set_window_size(1440, 900)
+    driver.set_window_position(0, 0)
     yield driver
     driver.quit()
