4162ca74c7c38683c2bee298e55ef5a618c94b98
nafis@aidocbuilder.com <rongdhong@54321>
2025-01-31 15:37:43 +0600

    linked batch opening functionality added

---
+++ pages/transaction_linked_batch.py
@@ -0,0 +1,34 @@
+import time
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from utils.logger import logger
+
+class LinkedBatchOpen:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def linked_batch_open_part(self):
+        try:
+            expand = WebDriverWait(self.driver, 10).until(
+                EC.visibility_of_element_located((By.XPATH, "(//div[@class='d-inline'])[1]"))
+            )
+            expand.click()
+            print("Expanded linked batch.")
+
+            linked_batch = WebDriverWait(self.driver, 10).until(
+                EC.visibility_of_element_located((By.XPATH,
+                                                  "/html[1]/body[1]/div[2]/div[1]/div[3]/div[3]/div[1]/div[1]/div[1]/div[1]/div[2]/table[1]/tbody[1]/tr[2]/td[1]/div[1]/div[1]/table[1]/tbody[1]/tr[2]/td[2]/a[1]"))
+            )
+            linked_batch.click()
+
+            logger.info("Waiting for the form control to be visible.")
+
+            WebDriverWait(self.driver, 15).until(
+                EC.visibility_of_element_located((By.CSS_SELECTOR, ".form-control"))
+            )
+            time.sleep(20)
+        except Exception as e:
+            logger.error(f"An error occurred while opening linked batch: {e}")
+            raise
